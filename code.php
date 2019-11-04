42aec383383659b0e4dcaa02719eadb0f540b65c
Tony Hansen <tony@digitalfire.com>
2019-11-03 19:16:48 -0700

    Update code.php

--- code.php
+++ code.php
@@ -1,3 +1,32 @@
+<?php
+if(isset($_POST) && $_POST) print_r($_POST);
+define('MAPFONT', 'arialbd.ttf');
+$background = imagecreatefromgif('samplemap.gif');
+$red = imagecolorallocate($background, 255, 0, 0);
+$white = imagecolorallocate($background, 255, 255, 255);
+$black = imagecolorallocate($background, 0, 0, 0);
+
+$data = file('mapdata.txt');
+foreach($data as $line) {
+	$t = explode("\t", $line);
+	$coords[] = array('label_points'=>intval($t[4]), 'x'=>intval($t[1]), 'y'=>intval($t[2]), 'label_angle'=>intval($t[3]), 'label'=>$t[0]);
+}
+# echo '<pre>'; print_r($coords); echo '</pre>'; die;
+foreach($coords as $n => $row) {
+	printit($row['label'], $background, $row, $black);
+	imagefilledellipse ($background, $row['x'], $row['y'], 8, 8, $red);
+}
+# create the image file to display
+imagejpeg($background, 'samplemap.jpg', 70);
+
+echo '<form method="POST" style="margin-top:0px" name="myform" action="samplemap.php">';
+echo '<input type="image" name="map" src="samplemap.jpg" style="margin-top:0px; cursor:crosshair;"/>';
+echo '</form>';
+
+echo '<pre>';
+echo htmlentities(file_get_contents('samplemap.php'));
+echo '</pre>';
+
 function calculateTextBox($fontSize,$fontAngle,$fontFile,$text) { 
     /************ 
     simple function that calculates the *exact* bounding box (single pixel precision). 
@@ -18,7 +47,7 @@ function calculateTextBox($fontSize,$fontAngle,$fontFile,$text) {
      "height" => $maxY - $minY, 
      "box"    => $rect 
     ); 
-}
+} 
 
 function printit($label, $background, $row, $color, $justify='C') {
     # justify would be center, left, right
@@ -27,21 +56,21 @@ function printit($label, $background, $row, $color, $justify='C') {
     if($label == '') $label = '??';
     $size = $row['label_points']; if($size==0) $size='20';
     $angle = $row['label_angle'];
-
+    
     if($angle < 0){
         $angle = 360 - ($angle*-1); //convert clockwise rotation to anti-clockwise
     }
     $angle = $angle % 360;  //for text rotation revolution is a cycle
     $y = $row['y'];
     $x = $row['x'];
-
+   
     # rotate cartesian coordinates calculator: http://keisan.casio.com/exec/system/1223522781
-
+   
     if($angle<=90) {
         $bbox = calculateTextBox($size, $angle, $font, $label);
         $height = $bbox['height'];
         $width = $bbox['width'];
-
+        
         $temp=imagettftext($background, $size, $angle, ($x-($width/2))+($angle/4),  $y+($height/2), $color, $font, $label);
         if($temp===false) imagestring($background, 5, 50, 50, 'error', $color);
     }
@@ -50,7 +79,7 @@ function printit($label, $background, $row, $color, $justify='C') {
         $height = $bbox['height'];
         $width = $bbox['width'];
         $temp=imagettftext($background, $size, $angle, ($x+($width/2)),  $y+($height/2)-5, $color, $font, $label);
-
+ 
         // $temp=imagettftext($background, $size, $angle, ($x-($height)),  $y+($width/2), $color, $font, $label);
         if($temp===false) imagestring($background, 5, 50, 50, 'error', $color);
     }
@@ -59,7 +88,7 @@ function printit($label, $background, $row, $color, $justify='C') {
         $height = $bbox['height'];
         $width = $bbox['width'];
         $temp=imagettftext($background, $size, $angle, ($x-($width/2))+ (270-$angle),  $y-($height/2), $color, $font, $label);
-
+ 
         // $temp=imagettftext($background, $size, $angle, ($x-($height)),  $y+($width/2), $color, $font, $label);
         if($temp===false) imagestring($background, 5, 50, 50, 'error', $color);
     }
@@ -68,9 +97,10 @@ function printit($label, $background, $row, $color, $justify='C') {
         $height = $bbox['height'];
         $width = $bbox['width'];
         $temp=imagettftext($background, $size, $angle, ($x-($width/2))+2,  $y-($height/2)+($angle/25), $color, $font, $label);
-
+ 
         // $temp=imagettftext($background, $size, $angle, ($x-($height)),  $y+($width/2), $color, $font, $label);
         if($temp===false) imagestring($background, 5, 50, 50, 'error', $color);
     }
     // if($temp===false) imagestring($background, 5, 50, 50, 'error', $color);
 }
+?>
